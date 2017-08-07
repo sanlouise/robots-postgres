4a28dd675a48608b37e8db62a7563f97bd688340
sanlouise <sandra.adamshallie@gmail.com>
2017-08-07 14:17:28 -0400

    Remove commented out table data

--- express-app.js
+++ express-app.js
@@ -1,26 +1,8 @@
 const express = require('express');
 const pgPromise = require('pg-promise')();
 const mustacheExpress = require('mustache-express');
-const robots = require('./robots');
 const pg = require('pg');
 const robotDB = pgPromise({ database: 'robots' })
-
-// CREATE TABLE robots (
-// id serial PRIMARY KEY,
-// username varchar(20) NOT NULL,
-// name varchar(40),
-// avatar varchar(150),
-// email varchar(50),
-// university varchar(80),
-// job varchar(100),
-// company varchar(80),
-// phone varchar(20),
-// street_num varchar(8),
-// street_name varchar(50),
-// city varchar(40),
-// state_or_province varchar(40),
-// postal_code varchar(10),
-// country varchar(40));
 
 const app = express();
 app.use(express.static('public'));
@@ -33,33 +15,30 @@ app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
 
-robotDB.any(`SELECT * from robots`).then(robots => {
+app.get('/', (request, response) => {
+  robotDB.any(`SELECT * from robots`)
+  .then(robots => {
     response.render('index', { robots })
   })
-})
-
-app.get('/', (request, response) => {
-  response.render('index', { robots });
 })
 
 app.get('/create', (request, response) => {
   response.render('create')
 })
 
-app.get('/info/:username', (request, response) => {
+app.get('/robot/:username', (request, response) => {
   const username = request.params.username;
-  console.log({username});
-  let user;
-  for (let i = 0; i < robots.length; i++) {
-    if (robots[i].username === username) {
-      user = robots[i];
-      console.log({user});
-      break;
-    }
-  }
-
-  if (!user) {
-    return response.status(404).send({ message: `Unable to find user ${username}.` });
-  }
-  return response.render('info', user);
+  robotDB.task('get-robot-info', task => {
+       return task.batch([
+          task.one('SELECT * FROM robots WHERE id = $(id)', { id: id }),
+       ])
+     })
+     .then(data => {
+         const robot = data
+         response.render("robot", {robot})
+     })
+       .catch(error => {
+         const displayError = "Oops, we can't seem to find that robot!"
+         response.render("robot", { displayError })
+     })
 })
